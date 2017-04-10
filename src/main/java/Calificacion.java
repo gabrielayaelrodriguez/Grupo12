e787ef1f94b25c9ed0189ddabd47775b10afd83f
Gabriela Yael Rodriguez <gabrielayaelrodriguez@gmail.com>
2017-04-10 15:52:08 -0300

    olaktal

---
+++ src/main/java/Calificacion.java
@@ -0,0 +1,18 @@
+
+public class Calificacion {
+int puntaje;
+
+public int getPuntaje() {
+	return puntaje;
+}
+public void setPuntaje(int puntaje) {
+	this.puntaje = puntaje;
+}
+public String getComentario() {
+	return comentario;
+}
+public void setComentario(String comentario) {
+	this.comentario = comentario;
+}
+String comentario;
+}
