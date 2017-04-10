c4e4aa40a804e1d527af5c71f280722aa9258f42
Gabriela Yael Rodriguez <gabrielayaelrodriguez@gmail.com>
2017-04-10 16:08:10 -0300

    dale nikoo

---
+++ src/main/java/Viaje.java
@@ -0,0 +1,47 @@
+import java.sql.Date;
+
+public class Viaje {
+	boolean finalizado;
+	String destino;
+	String origen;
+	float costo;
+	Date fecha;
+	public boolean isFinalizado() {
+		return finalizado;
+	}
+	public void setFinalizado(boolean finalizado) {
+		this.finalizado = finalizado;
+	}
+	public String getDestino() {
+		return destino;
+	}
+	public void setDestino(String destino) {
+		this.destino = destino;
+	}
+	public String getOrigen() {
+		return origen;
+	}
+	public void setOrigen(String origen) {
+		this.origen = origen;
+	}
+	public float getCosto() {
+		return costo;
+	}
+	public void setCosto(float costo) {
+		this.costo = costo;
+	}
+	public Date getFecha() {
+		return fecha;
+	}
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+	public int getMax_pasajeros() {
+		return max_pasajeros;
+	}
+	public void setMax_pasajeros(int max_pasajeros) {
+		this.max_pasajeros = max_pasajeros;
+	}
+	int max_pasajeros;
+
+}
