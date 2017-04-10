08a250907fb33541d4df5bbcbd73bfe1ef099681
Gabriela Yael Rodriguez <gabrielayaelrodriguez@gmail.com>
2017-04-10 16:03:10 -0300

    jdflksd

--- src/main/java/Usuario.java
+++ src/main/java/Usuario.java
@@ -1,4 +1,25 @@
+import java.sql.Date;
 
 public class Usuario {
-
+String nombre;
+String contraseña;
+Date f_ingreso;
+public String getNombre() {
+	return nombre;
+}
+public void setNombre(String nombre) {
+	this.nombre = nombre;
+}
+public String getContraseña() {
+	return contraseña;
+}
+public void setContraseña(String contraseña) {
+	this.contraseña = contraseña;
+}
+public Date getF_ingreso() {
+	return f_ingreso;
+}
+public void setF_ingreso(Date f_ingreso) {
+	this.f_ingreso = f_ingreso;
+}
 }
