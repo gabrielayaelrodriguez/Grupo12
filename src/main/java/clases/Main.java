7d17f774c68a4328cf38d4fec2c210973a91eec9
Gabriela Yael Rodriguez <gabrielayaelrodriguez@gmail.com>
2017-04-16 13:44:23 -0300

    fks

--- src/main/java/clases/Main.java
+++ src/main/java/clases/Main.java
@@ -2,34 +2,36 @@ package clases;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.service.ServiceRegistry;
-import org.hibernate.service.ServiceRegistryBuilder;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.tool.hbm2ddl.SchemaExport;
+import org.hibernate.service.ServiceRegistry;
 
 public class Main {
 	
 	
 	public static void main(String[] args) {
 		
+		
+		Configuration conf = new Configuration().configure();
+		ServiceRegistry sr = new StandardServiceRegistryBuilder().applySettings(conf.getProperties()).build();
+		SessionFactory sf = conf.buildSessionFactory(sr);
+		Session session = sf.openSession();
+		session.beginTransaction();
+		
 		//SessionFactory sessionFactory;
 		
 				// Setting up Hibernate
-		        System.out.println("aca arranca");
-				Configuration configuration = new Configuration();
-				configuration.configure("/hibernate/hibernate.cfg.xml");
+		        //System.out.println("aca arranca");
+				//Configuration configuration = new Configuration();
+				//configuration.configure("/hibernate/hibernate.cfg.xml");
 				//borra si existe el esquema en la base de datos asi no hay conflictos y asi deja libre para cargar la nueva version de las tablas
-				System.out.println("drop");
-				new SchemaExport(configuration).drop(true, true);
-				System.out.println("funca drop.");
+				//System.out.println("Droping schema.........");
+				//new SchemaExport(configuration).drop(true, true);
+				//System.out.println("DONE.");
 				//genera las tablas
-				System.out.println("create");
-				new SchemaExport(configuration).create(true, true);
-				System.out.println("funca create.");
-				
-				
-				
-				
+				//System.out.println("Generating schema.........");
+				//new SchemaExport(configuration).create(true, true);
+				//System.out.println("DONE.");
 				/*ServiceRegistry serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();
 				sessionFactory = configuration.buildSessionFactory(serviceRegistry);
 				//Droping schema
@@ -51,13 +53,11 @@ public class Main {
 				session.getTransaction().commit();
 				session.close();
 				*/
-				System.out.println(org.hibernate.Version.getVersionString());
-				System.out.println("llegamos a la carg.");
+				
 				UsuariosDAO usuariosDAO = new UsuariosDAO();
 				Pasajero user1=new Pasajero("pepe", "123", 700);  //Creamos el objeto
 				System.out.println(user1.getNombre());
-				usuariosDAO.guardarUsuario(user1);
-				System.out.println("cargo.");
+				//usuariosDAO.guardarUsuario(user1);
 				
 				
 	}
